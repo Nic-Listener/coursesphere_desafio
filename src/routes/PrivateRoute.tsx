e5f385ac3085b6bedffe4df501e07161c4624384
Nicolas Fernandes <nicolados10@gmail.com>
2025-06-25 07:51:27 -0300

    feat: add proteção e config das rotas

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,13 @@
+import React, { JSX } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+interface PrivateRouteProps {
+  children: JSX.Element;
+}
+
+export const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
