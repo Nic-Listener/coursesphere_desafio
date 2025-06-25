c37666d0a4bef4bda6b3c16157296144ea36a54e
Nicolas Fernandes <nicolados10@gmail.com>
2025-06-25 20:45:30 -0300

    feat: add serviço para os cursos

---
+++ src/services/CourseService.ts
@@ -0,0 +1,22 @@
+import { api } from '../api/api';
+
+export interface Course {
+  id: number;
+  name: string;
+  description?: string;
+  start_date: string;
+  end_date: string;
+  creator_id: number;
+  instructors: number[];
+}
+
+export const getCoursesByUser = async (userId: number): Promise<Course[]> => {
+  const response = await api.get<Course[]>('/courses', {
+    params: {
+      q: userId
+    }
+  });
+  return response.data.filter(
+    (course) => course.creator_id === userId || course.instructors.includes(userId)
+  );
+};
